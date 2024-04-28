f283ce0216f2fdb1d61c3ce1dd75cd9ef4de28e1
Michał Dobranowski <mdbrnowski@gmail.com>
2024-04-28 23:53:23 +0200

    Add another function description

--- lib/elixir_challenge/rho.ex
+++ lib/elixir_challenge/rho.ex
@@ -45,6 +45,9 @@ defmodule ElixirChallenge.Rho do
   #   end
   # end
 
+  @doc """
+  Pollard's rho algorithm.
+  """
   @spec pollard_rho(pos_integer()) :: pos_integer()
   def pollard_rho(n) do
     if n == 1 or prime?(n) do
